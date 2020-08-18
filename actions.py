410c50cbbf237a82a62554bac1942cbc7d9c7888
Monalisa <monalisa.kk19@gmail.com>
2020-08-18 19:20:31 +0530

    First upload

---
+++ actions.py
@@ -0,0 +1,61 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+import requests
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionNewTravelCard(Action):
+    def name(self) -> Text:
+        return "action_newtravelcard"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Please go to www.google.com to open the instruction document to apply new card")
+
+        return []
+    
+class ActionStatusCard(Action):
+    def name(self) -> Text:
+        return "action_statuscard"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Please provide the SR ticket #")
+
+        return []
+
+class ActionIncidentTable(Action):
+    def name(self) -> Text:
+        return "action_Incident"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="Please provide your query in detail")
+
+        return []
---
+++ data/nlu.md
@@ -0,0 +1,74 @@
+## intent:greet
+- hey
+- hello
+- hi
+- heya
+- good morning
+- good evening
+- hey there
+
+## intent:newtravelcard
+- Apply for new Travel card
+- New travel card
+- New card
+
+## intent:statuscard
+- status of card 
+- applied card status 
+- updates on applied card
+
+## intent:createticket
+- create ticket
+- raise ticket
+- incident
+- create incident
+
+
+## intent:goodbye
+- bye
+- goodbye
+- see you around
+- see you later
+
+## intent:affirm
+- yes
+- indeed
+- of course
+- that sounds good
+- correct
+
+## intent:deny
+- no
+- never
+- I don't think so
+- don't like that
+- no way
+- not really
+
+## intent:mood_great
+- perfect
+- very good
+- great
+- amazing
+- wonderful
+- I am feeling very good
+- I am great
+- I'm good
+
+## intent:mood_unhappy
+- sad
+- very sad
+- unhappy
+- bad
+- very bad
+- awful
+- terrible
+- not very good
+- extremely sad
+- so sad
+
+## intent:bot_challenge
+- are you a bot?
+- are you a human?
+- am I talking to a bot?
+- am I talking to a human?
